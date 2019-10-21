0f4515840a3c0070fb5543930ed1b11f37fd1119
gawaxx <jules.blanc@unccub.com>
2019-10-21 09:46:03 +0100

    Wrote more relationships

--- db/migrate/003_create_review.rb
+++ db/migrate/003_create_review.rb
@@ -1,23 +1,25 @@
-class CreateReview < ActiveRecord::Migration
+class CreateReview < ActiveRecord::Migration[6.0]
 #This is the database for my reviews 3
 def change
-    create_table :user do |t|
-      t.string :name
-      t.string :email
-      t.string :age 
-      t.timestamps
-    end
+    #create_table :user do |t|
+      #t.string :name
+      #t.string :email
+      #t.string :age 
+      #t.timestamps
+    #end
  
-    create_table :hotel do |t|
-      t.string :name
-      t.string :email
-      t.string :location
-      t.timestamps
-    end
+    #create_table :hotel do |t|
+     # t.string :name
+     # t.string :email
+     # t.string :location
+     # t.timestamps
+    #end
  
     create_table :review do |t|
       t.belongs_to :user
       t.belongs_to :hotel
+      t.string :user_id
+      t.string :hotel_id 
       t.string :title 
       t.string :rating
       t.timestamps
