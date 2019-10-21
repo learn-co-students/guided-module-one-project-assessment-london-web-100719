1f511be67eb902fb134d18ab257fc70791c6687a
gawaxx <jules.blanc@unccub.com>
2019-10-21 11:49:36 +0100

    All seed data is pretty much setup for MVP, moving on to CLI

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 10.times do 
-    User.create(name: Faker::Name.name, age: Faker::Number.number(digits: 2))
+    User.create(name: Faker::Name.name, age: Faker::Number.number(digits: 2), email: Faker::Internet.unique.email)
 end 
 
 10.times do 
-    Hotel.create(name: Faker::Company.name)
+    Hotel.create(name: Faker::Company.unique.name, email: Faker::Internet.unique.email, location: Faker::Address.unique.street_address , phone_number: Faker::PhoneNumber.cell_phone)
 end 
 
 10.times do 
-    Review.create(users_id: User.all.sample.id, hotels_id: Hotel.all.sample.id, content: Faker::Hacker.say_something_smart)
+    Review.create(users_id: User.all.sample.id, hotels_id: Hotel.all.sample.id, title: Faker::Hacker.say_something_smart, content: Faker::Hacker.say_something_smart)
 end 
